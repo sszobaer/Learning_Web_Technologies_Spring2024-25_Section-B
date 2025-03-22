0804e9775b2763a24a94dbff9296fd1cc7e094a4
sszobaer <22-49415-3@student.aiub.edu>
2025-03-22 14:50:57 +0600

    JS practice file uploaded

---
+++ Javascript Practice/scripts.js
@@ -0,0 +1,23 @@
+let validation = ()=>{
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+
+    if(email == ""){
+        alert("Please enter the email adrees");
+        return false;
+    }
+    else if (password == ""){
+        alert("Please anter a valid password");
+        return false;
+    }
+    else if(email == password){
+        alert("email and password should not be matched");
+        return false;
+    }
+    else{
+        const emailHome = document.getElementById('emailHome');
+        const passwordHome = document.getElementById('passwordHome');   
+    }
+
+    return true;
+}
